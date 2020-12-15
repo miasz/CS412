7a1bb06dbc587727d778c4864c620620aa874a4b
miasz <43791471+miasz@users.noreply.github.com>
2020-12-15 17:36:10 -0600

    Add files via upload

---
+++ routes/PA3.js
@@ -0,0 +1,21 @@
+// PA3
+// Mia Szela
+// U12890497
+
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/',(req, res, next) => {
+    console.log(`Got ${req.query.name} and ${req.query.age} on ${req.url} with (${req.method})`)
+    res.render('ps3', {'name': req.query.name, 'age': req.query.age})
+});
+
+router.post('/',(req, res, next) => {
+    console.log(`Got ${req.body.name} and ${req.body.age} on ${req.url} with (${req.method})`)
+    res.render('ps3', {'name': req.body.name, 'age': req.body.age,
+        'length':req.body.name.length, 'isPost': true})
+});
+
+module.exports = router;
